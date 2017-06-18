c375347d348f8dfc1141a370646ffad01baae95b
Mark <suyatdesign@gmail.com>
2017-06-17 19:57:06 -0700

    refactored models for links and users to Mongoose

--- app/models/link.js
+++ app/models/link.js
@@ -1,19 +1,29 @@
-var db = require('../config');
+// var db = require('../config');
 var crypto = require('crypto');
+var mongoose = require('mongoose');
 
-var Link = db.Model.extend({
-  tableName: 'urls',
-  hasTimestamps: true,
-  defaults: {
-    visits: 0
-  },
-  initialize: function() {
-    this.on('creating', function(model, attrs, options) {
-      var shasum = crypto.createHash('sha1');
-      shasum.update(model.get('url'));
-      model.set('code', shasum.digest('hex').slice(0, 5));
-    });
-  }
+var linkSchema = mongoose.Schema({
+  visits: Number,
+  title: String,
+  code: String,
+  baseUrl: String,
+  url: String
+});
+
+var Link = mongoose.model('Link', linkSchema);
+
+var createSha = function(url) {
+  console.log('CreateShaw for URL', url)
+  var shasum = crypto.createHash('sha1');
+  shasum.update(url);
+  return shasum.digest('hex').slice(0, 5);
+};
+
+linkSchema.pre('save', function(next) {
+  console.log('pre validate');
+  var code = createSha(this.url);
+  this.code = code;
+  next();
 });
 
 module.exports = Link;
